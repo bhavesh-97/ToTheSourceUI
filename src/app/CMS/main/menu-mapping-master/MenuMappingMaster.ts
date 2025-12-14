ef45f0f470fa901cbe3d6230a73f75c40752ae7c
Bhavesh MotiBhai Vaghela <vaghelabhaveshm3@gmail.com>
2025-12-14 22:10:38 +0530

    commit

--- src/app/CMS/main/menu-mapping-master/MenuMappingMaster.ts
+++ src/app/CMS/main/menu-mapping-master/MenuMappingMaster.ts
@@ -1,9 +1,10 @@
 import { MCommonEntitiesMaster } from "../../../models/MCommonEntitiesMaster";
 
 export class MMenuMappingMaster {
+  MappingID: number = 0;
   MenuID: number = 0;
-  MenuName: string = "";
-  MenuURL: string = "";
-  Icon: string = "";
+  MenuType: number = 0;
+  ParentID: number = 0;
+  MenuRank: number = 0;
   MCommonEntitiesMaster: MCommonEntitiesMaster = new MCommonEntitiesMaster();
 }
