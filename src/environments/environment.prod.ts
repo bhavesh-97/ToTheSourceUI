b0159935a5425f767b7f78f9afe9dfc245bdff19
Bhavesh MotiBhai Vaghela <exegil-sw86@gujarat.gov.in>
2026-06-22 15:10:48 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,8 @@
 export const environment = {
   production: true,
-  environmentName: 'Localhost',  
-  reportServerUrl: 'https://chennaicbskendodev.amnex.com',
-  //pnsUrl: 'https://localhost:44398', //local
-  //pnsUrl: 'http://10.195.96.148:6017'//live
-  pnsUrl: 'https://chennaimasterdevapi.amnex.com/pns'
+  environmentName: '100.82.237.11',  
+  CMSUrl: 'http://100.82.237.11:5000/CMS', 
+  WebUrl: 'http://100.82.237.11:5000', 
 };
 
 export const ExportExcelLogoCellMerge: string = "A1:A3";
