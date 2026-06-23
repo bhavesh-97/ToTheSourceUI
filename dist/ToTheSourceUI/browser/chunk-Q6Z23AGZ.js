3d994039a913a34fa66cc85ec007649e26dfedaa
Bhavesh MotiBhai Vaghela <exegil-sw86@gujarat.gov.in>
2026-06-23 14:39:08 +0530

    commit dist

--- dist/ToTheSourceUI/browser/chunk-Q6Z23AGZ.js
+++
@@ -1,1 +0,0 @@
-var e=class{isActive=!1;createdBy=0;createdDate=new Date;updatedBy=0;updatedDate=new Date;isDeleted=!1;deletedBy=0;deletedDate=new Date};export{e as a};
--- dist/ToTheSourceUI/browser/chunk-X2IVQF4C.js
+++
@@ -1,1 +0,0 @@
-var s=class{static userInfo=null;static get userId(){return this.userInfo?.userID??0}};export{s as a};
